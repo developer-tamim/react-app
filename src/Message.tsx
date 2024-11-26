8e31cc018b2dba87ac26cb4362db1fa8be40d072
developer-tamim <tawsiftamim24@gmail.com>
2024-11-26 11:07:03 +0600

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing
+function Message(){
+    // JSX: JavaScript XML
+    const name = "Reat"
+    if (name)
+        return <h1>{name} Component</h1>;
+    return <h1>Message Component</h1>;
+}
+
+export default Message;
