375e8039003252750cf03156cb4843a18f409727
developer-tamim <tawsiftamim24@gmail.com>
2024-12-06 11:24:35 +0600

    passing data,function by props

--- src/component/ListGroup.tsx
+++ src/component/ListGroup.tsx
@@ -1,7 +1,13 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
+//{items:[], heading: string,}
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   // items = [];
 
   // if (items.length === 0)
@@ -18,20 +24,23 @@ function ListGroup() {
   //   return items.length === 0 ? <p>No items found</p> : null;
   // }
 
-
-  const handleClick = (event: MouseEvent) => console.log(event);
-
   return (
     <>
-      <h1>List</h1>
-      {/* {items.length === 0 ? <p>No items found</p> : null} */}
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item"
-            onClick={handleClick}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item)
+            }}
           >
             {item}
           </li>
